2a911ba454b03c2fad48829632d18c512b81ce33
Nithin Prasad <nithin2889@gmail.com>
2022-03-23 00:46:34 +0530

    refresh scope for all microservices

--- accounts/src/main/java/com/dawn/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/dawn/accounts/AccountsApplication.java
@@ -3,11 +3,13 @@ package com.dawn.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({@ComponentScan("com.dawn.accounts.controller")})
 @EnableJpaRepositories("com.dawn.accounts.repositories")
 @EntityScan("com.dawn.accounts.model")
--- loans/src/main/java/com/dawn/loans/LoansApplication.java
+++ loans/src/main/java/com/dawn/loans/LoansApplication.java
@@ -3,11 +3,13 @@ package com.dawn.loans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({@ComponentScan("com.dawn.loans.controller")})
 @EnableJpaRepositories("com.dawn.loans.repository")
 @EntityScan("com.dawn.loans.model")
